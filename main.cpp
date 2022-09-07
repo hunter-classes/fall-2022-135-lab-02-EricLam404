3e2447afa564bf6f693e84b515fe957541ee3864
EricLam404 <lameric9888@gmail.com>
2022-09-07 19:19:07 -0400

    Added Task B

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "funcs.h"
+
+int main(){
+
+  print_interval(0, 10);
+  std::cout << "\n";
+
+  print_interval(-10,0);
+  std::cout << "\n";
+  
+  print_interval(-10,10);
+  std::cout << "\n";
+  
+}
